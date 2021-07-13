fb04ac13b223aed5c19657889c115fe193a56262
Jelena <jelena.durovic@powerhrg.com>
2021-07-12 21:15:53 -0400

    commit tree bcd4e06935bbec3bce8b66f75878dc3548bb0c6d

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+import { data } from "browserslist";
+import React, { useEffect, useState} from "react"
+
+const App = () => {
+    const [image, setImage] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => {
+              console.log(data)
+            setImage(data.message);
+            setIsLoaded(true);
+          });
+      }, []);
+       // if the data hasn't been loaded, show a loading indicator
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return <img src={image} alt="A Random Dog"/>;
+}
+
+
+
+
+
+
+
+
+
+
+
+export default App
